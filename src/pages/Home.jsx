56b6eb17dd89e8b1627334d975a01d7d27aa7e6b
zhasaleh <akhaybala221@gmail.com>
2024-06-05 21:55:19 +0800

    Menambahkan File Klinik App2

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import UserLayout from '../components/UserLayout'
+
+
+export default function Home() {
+  return (
+    <UserLayout>
+      <button className="btn btn-warning">Button contoh</button>
+    </UserLayout>
+  )
+}
